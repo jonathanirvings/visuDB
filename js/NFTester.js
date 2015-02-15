5cb28e1f0e6b1db4746ab3e3596fb8085b33c926
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2015-02-15 15:49:36 +0700

    Added skeleton of NFTester

---
+++ js/NFTester.js
@@ -0,0 +1,18 @@
+function NFTester() {
+}
+
+NFTester.prototype.2NFTest = function(relation) {
+	return true;
+}
+
+NFTester.prototype.3NFTest = function(relation) {
+	return true;
+}
+
+NFTester.prototype.EKNFTest = function(relation) {
+	return true;
+}
+
+NFTester.prototype.BCNFTest = function(relation) {
+	return true;
+}
