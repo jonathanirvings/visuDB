f67d77d4dbc42f685a064b9c173dbf6ad65ebc3c
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2015-03-11 16:12:04 +0800

    Added union in utlityFunction

--- js/Utility.js
+++ js/Utility.js
@@ -25,4 +25,27 @@ Utility.isProperSubset = function(left, right) {
     return true;
 }
 
+//return an array which the union of left and right
+Utility.union = function(left,right) {
+    var union = new Array();
+    for (var i = 0; i < left.length; ++i) {
+        union.push(left[i]);
+    }
+    for (var i = 0; i < right.length; ++i) {
+        var isExist = false;
+        for (var j = 0; j < union.length; ++j) {
+            if (right[i] == union[j]) {
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            union.push(right[i]);
+        }
+    }
+    return union;
+}
+
+
+
 
